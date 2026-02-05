af952989504d558cbf9d04928b530fef6b9e397d
Suchitra51 <panigrahisuchitra51@gmail.com>
2026-02-05 12:13:19 +0530

    Added react-routing-app as a normal folder

---
+++ react-routing-app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This page gives information about the application.</p>
+    </div>
+  );
+}
+
+export default About;
