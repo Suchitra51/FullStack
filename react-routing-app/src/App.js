8ddf94362396c438936dad997be0657a46fcef86
Suchitra51 <panigrahisuchitra51@gmail.com>
2026-02-23 22:06:04 +0530

    Added Experiment-4 and updated react-routing-app

--- react-routing-app/src/App.js
+++ react-routing-app/src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+
 import Home from "./pages/Home";
-import About from "./pages/About";
+import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div>
-      <h1>React Client-Side Routing</h1>
+      <h1>Navigation Using Link Component</h1>
 
+      {/* Navigation Bar */}
       <nav>
         <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
+        <Link to="/services">Services</Link> |{" "}
         <Link to="/contact">Contact</Link>
       </nav>
 
+      {/* Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
